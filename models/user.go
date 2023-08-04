f5b96ee251a8908d436c4cc851012d4bec4b3463
zhouzhou <1197675873@qq.com>
2023-08-04 13:08:19 +0800

    init:construction:: init video and comment models

--- models/user.go
+++ models/user.go
@@ -22,4 +22,10 @@ type User struct {
 	SendMessage []Message `gorm:"foreignKey:from_user_id"`
 	//  收到消息
 	ReceiveMessage []Message `gorm:"foreignKey:to_user_id"`
+	//  发表的评论
+	Commnet []Comment
+	//  发布的视频
+	CreatedVideo []Video `gorm:"foreignKey:AuthorID"`
+	//  喜欢的视频
+	LikeVideo []Video `gorm:"many2many:video_likes"` // many2many关系 连接表名video_likes
 }
