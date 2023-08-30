c9944a7ba21b0ef0a9cbf3985857e6dfae2e74ea
yumu <2816083598@qq.com>
2023-08-30 19:35:15 +0800

    fix:bug:: update videosevice function

--- service/VideoService.go
+++ service/VideoService.go
@@ -4,7 +4,6 @@ import (
 	"douyin/models"
 	"fmt"
 	"strconv"
-	"time"
 )
 
 // MaxVideoNum 每次最多返回的视频流数量
@@ -12,21 +11,7 @@ const (
 	MaxVideoNum = 30
 )
 
-type FeedVideoList struct {
-	Videos   []*models.Video `json:"video_list,omitempty"`
-	NextTime int64           `json:"next_time,omitempty"`
-}
-
-type QueryFeedVideoListFlow struct {
-	userId     uint
-	latestTime time.Time
-	videos     []*models.Video
-	nextTime   int64
-	feedVideo  *FeedVideoList
-}
-
 // redis 缓存查询
-
 
 func GetVideoCommentCount(v *models.VideoInfo) error {
 	commentCount, err := models.RedisClient.LLen(RedisCtx, INTERACT_COMMENT_KEY+strconv.Itoa(int(v.ID))).Result()
@@ -51,13 +36,31 @@ func GetVideoInfoByIds(curId uint, vids []uint) {
 
 }
 
-
 // =============================================================================================================
 
 func GetVideoById(vid uint) (models.Video, error) {
 	video := models.Video{}
 	err := models.DB.First(&video, vid).Error
 	return video, err
+}
+
+func GetVideosByUpdateAt() ([]models.Video, error) {
+	videos := make([]models.Video, 10)
+	err := models.DB.Order("updated_at desc").Find(&videos).Error
+	if err != nil {
+		return nil, err
+	}
+	return videos, err
+}
+
+func GetVideosLenght() (int64, error) {
+	var video models.Video
+	var lenght int64
+	err := models.DB.Model(&video).Count(&lenght).Error
+	if err != nil {
+		return lenght, err
+	}
+	return lenght, err
 }
 
 // func GetVideosByIds(vids []uint) ([]models.Video, error) {
@@ -78,41 +81,4 @@ func GetVideosByUserId(uid uint) ([]models.Video, error) {
 
 func CreateVideo(video models.Video) error {
 	return models.DB.Create(&video).Error
-}
-
-// =============================================================================================================// =============================================================================================================// =============================================================================================================
-
-func QueryFeedVideoList(userId uint, latestTime time.Time) (*FeedVideoList, error) {
-	return NewQueryFeedVideoListFlow(userId, latestTime).Do()
-}
-
-func NewQueryFeedVideoListFlow(userId uint, latestTime time.Time) *QueryFeedVideoListFlow {
-	return &QueryFeedVideoListFlow{userId: userId, latestTime: latestTime}
-}
-
-func (q *QueryFeedVideoListFlow) Do() (*FeedVideoList, error) {
-	//所有传入的参数不填也应该给他正常处理
-	q.checkNum()
-	if err := q.packData(); err != nil {
-		return nil, err
-	}
-	return q.feedVideo, nil
-}
-
-func (q *QueryFeedVideoListFlow) checkNum() {
-	//上层通过把userId置零，表示userId不存在或不需要
-	if q.userId > 0 {
-		// 用户Id是有效的
-	}
-	if q.latestTime.IsZero() {
-		q.latestTime = time.Now()
-	}
-}
-
-func (q *QueryFeedVideoListFlow) packData() error {
-	q.feedVideo = &FeedVideoList{
-		Videos:   q.videos,
-		NextTime: q.nextTime,
-	}
-	return nil
 }
